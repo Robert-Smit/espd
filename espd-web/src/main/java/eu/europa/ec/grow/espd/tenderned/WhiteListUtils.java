cafa1af6ebbeb19bda284099210a9cc73175fdca
Desiree Hof <d.hof@dictu.nl>
2016-09-06 12:48:10 +0200

    TNR-8684 / TNR-9464 - Whitelist gemaakt

---
+++ espd-web/src/main/java/eu/europa/ec/grow/espd/tenderned/WhiteListUtils.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2009-2016 PIANOo; TenderNed programma.
+ */
+package eu.europa.ec.grow.espd.tenderned;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * espd - Description.
+ *
+ * @author D Hof
+ * @since 05-09-2016
+ */
+
+@Configuration
+@Component
+@PropertySource("classpath:whitelist.properties")
+public class WhiteListUtils {
+
+    @Value("${whitelist.url}")
+    private String whiteList;
+
+    @Bean
+    public static PropertyPlaceholderConfigurer properties(){
+        PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
+        Resource[] resources = new ClassPathResource[ ]
+                { new ClassPathResource( "whitelist.properties" ) };
+        ppc.setLocations( resources );
+        ppc.setIgnoreUnresolvablePlaceholders( true );
+        return ppc;
+    }
+
+    public void setWhiteList(String whiteList) {
+        this.whiteList = whiteList;
+    }
+
+    public List<String> getWhiteList() {
+        ArrayList<String> wordArrayList = new ArrayList<>();
+        for(String word : whiteList.split(",")) {
+            wordArrayList.add(word);
+        }
+        return wordArrayList;
+    }
+
+}
